fb3283daae012ec06724e54d8a7471ce63b30a1f
Brian Lovell <lovell.brian@gmail.com>
2023-08-24 00:23:44 +0000

    Move most of the installs to Dockerfile

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -8,21 +8,21 @@
 # python3 -m pip install --upgrade pip
 # pip3 install --user -r .devcontainer/requirements.txt
 # update CUDA Linux GPG repository key
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
-dpkg -i cuda-keyring_1.0-1_all.deb
-rm cuda-keyring_1.0-1_all.deb
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+# dpkg -i cuda-keyring_1.0-1_all.deb
+# rm cuda-keyring_1.0-1_all.deb
 # install cuDNN
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
-add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" -y
-apt-get update
-apt-get install libcudnn8=8.9.0.*-1+cuda11.8
-apt-get install libcudnn8-dev=8.9.0.*-1+cuda11.8
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+# mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
+# add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" -y
+# apt-get update
+# apt-get install libcudnn8=8.9.0.*-1+cuda11.8
+# apt-get install libcudnn8-dev=8.9.0.*-1+cuda11.8
 # install recommended packages
 apt-get install zlib1g g++ freeglut3-dev \
     libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage-dev -y
 # clean up
 pip3 cache purge
-apt-get autoremove -y
-apt-get clean
+sudo apt-get autoremove -y
+sudo apt-get clean
